d4a222689493c49cd5216887ea63735f3517c2da
Yurii M <mokruy9999@gmail.com>
2026-04-04 17:14:31 +0200

    refactor: migrate project structure to Nuxt 4 directory layout and update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  future: {
+    compatibilityVersion: 4,
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
-  css: ['~/assets/css/main.css']
+  css: ['~/assets/css/main.css'],
 })
